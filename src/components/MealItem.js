c74124e41cadc4a3efbc4737545893cb367c84ba
Kullike Annuk <kullike.annuk@voco.ee>
2025-04-04 10:21:08 +0300

    Ülesanne 5 - Valuuta andmete kuvamine

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -5,7 +5,7 @@ const MealItem = (props) => {
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-price">{new Intl.NumberFormat("et-ET", {style:"currency", currency: "EUR"}).format( props.meal.price,)}</p>
                     <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p className="meal-item-actions">

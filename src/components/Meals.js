8647232b59c3c9097639788044fa21325ce22d31
Kullike Annuk <kullike.annuk@voco.ee>
2025-04-04 09:53:34 +0300

    Ülesanne 3 - andmete pärimine Meals.js komponendis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react" 
+
 const Meals = () => {
+    const [meals, serMeals] = useState([]);
+    
+    useEffect(() => {
+        async function fetchMeals() {
+            fetch("http://localhost:3001/meals")
+            .then(response => response.json())
+            .then(data => serMeals(data))
+        }
+        fetchMeals()
+    }, [])
+console.log(meals)
     return (
         <ul id="meals">
             { 
